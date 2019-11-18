6707a641b9aba4f2073bcf9b469346a981caa73a
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2019-11-18 12:35:35 +0300

    sentryUi is added, and is served staticly

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { GatewayModule } from './gateway/gateway.module';
 import { PageModule } from './page/page.module';
 import { FragmentModule } from './fragment/fragment.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join, dirname } from 'path';
+
+const sentryUi = dirname(require.resolve('@puzzle-js/sentry-ui'));
 
 @Module({
-  imports: [GatewayModule, PageModule, FragmentModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: sentryUi,
+    }),
+    GatewayModule,
+    PageModule,
+    FragmentModule,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
